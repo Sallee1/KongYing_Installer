b2c66a9828c9dad416b7745a4f563b7597365acf
Sallee1 <885331635@qq.com>
2023-04-02 18:07:01 +0800

    fix: config重定义问题

--- Header Files/config.h
+++ Header Files/config.h
@@ -11,17 +11,18 @@ public:
     wstring publisher=L"空荧酒馆";
     wstring displayIcon=L"map.exe";
     wstring uninstallString=L"Uninstall.exe";
+    wstring InstallLocation = L"";        //自动配置，留空
     DWORD estimatedSize = 342016;
   } RegInfo;
 
   typedef struct InstallInfo{
-    wstring installPath;
-    bool desktopShortcut;
-    bool startmenuShortcut;
+    wstring defaultInstallPath = L"C:\\Program Files\\KongYingMap";
+    bool desktopShortcut = true;
+    bool startmenuShortcut = true;
 
     wstring exePath = L"map.exe";
     //放置用来覆盖安装检测的文件名(夹)，同时检测到文件存在则采取覆盖安装策略
-    std::vector<wstring> existFileName = { L"map.exe" ,L"UnityCrashHandler64.exe",L"UnityPlayer.dll"};    
+    std::vector<wstring> existFileName = { L"Map.exe" ,L"UnityCrashHandler64.exe",L"UnityPlayer.dll"};    
   } InstallInfo;
 
 public:
@@ -30,5 +31,5 @@ public:
 };
 
 
-config::RegInfo config::reginfo;
-config::InstallInfo config::installInfo;
+inline config::RegInfo config::reginfo;
+inline config::InstallInfo config::installInfo;
