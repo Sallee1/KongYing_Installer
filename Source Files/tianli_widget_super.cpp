6341e1bb5c48de8a2d1e90a08fc2cbe201b1cfae
Sallee1 <885331635@qq.com>
2023-04-09 17:15:54 +0800

    fix:修复超链接

--- Source Files/tianli_widget_super.cpp
+++ Source Files/tianli_widget_super.cpp
@@ -33,7 +33,9 @@ namespace tianli {
     mainShadow_B->setColor(QColor(255, 0, 0));
     mainShadow_B->setBlurRadius(14);
     ui->label_MainShadow_B->setGraphicsEffect(mainShadow_B);
-
+    //允许链接
+    ui->label_Readme->setOpenExternalLinks(true);
+    ui->label_Readme_Update->setOpenExternalLinks(true);
     //加载标题字体
     tianliUtils::setFont(":/font/Resource/fonts/SOURCEHANSERIFSC-HEAVY.ttf", ui->label_Title, true);   //标题 思源宋体
   }
